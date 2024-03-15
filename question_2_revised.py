81ee5eea1547ee1f40d0fe0e919ba6858a0d8474
Davy Jones <kunal.dekhane@gmail.com>
2024-03-15 13:22:08 -0400

    Committing changes March 15th

--- question_2_revised.py
+++ question_2_revised.py
@@ -9,74 +9,142 @@ class Vector3D:
 
     # The class should have three private data members to represent position in x,y,z coordinates.
     def __init__(self, x,y,z) -> None:
+        """
+            Constructor to initialize the 3 variables representing coordinates of the 3D Vector
+
+                Parameters:
+                    The x, y, z coordinates of the 3D vector
+
+                Returns:
+                    None
+        """
         self.x = x
         self.y = y
         self.z = z
 
-    # private data variables here
 
-    # The class should have public member functions that perform the following:
-    #
-    # Vector3D() - constructor
-    #
+
     # cross()    - returns the cross product of itself with another vector
 
     def cross(self, b):
+        """
+            Calculates the cross product of the vector in its instance with the vector passed
+
+                Parameters:
+                    The x, y, z coordinates of the second 3D vector
+
+                Returns:
+                    Calculated cross product of the two vectors
+        """
         a = (self.x, self.y, self.z)
 
-        cross_product = [a[1] * b[2] - a[2] * b[1],
+        try:
+            cross_product = [a[1] * b[2] - a[2] * b[1],
              a[2] * b[0] - a[0] * b[2],
              a[0] * b[1] - a[1] * b[0]]
+            print("The cross product of these vectors is: ", cross_product)
 
-        return cross_product
+            return cross_product
+        except IndexError:
+            print("IndexError (cross): You did not enter the values in the correct format. Please run the program again")
 
 
     # dot()      - returns the dot product of itself with another vector
     def dot(self, b):
+        """
+            Calculates the dot product of the vector in its instance with the vector passed
+
+                Parameters:
+                    The x, y, z coordinates of the second 3D vector
+
+                Returns:
+                    Calculated dot product of the two vectors
+        """
         a = (self.x, self.y, self.z)
-        dot_product = sum(x * y for x, y in zip(a, b))  # + 100
+        try:
+            dot_product = sum(x * y for x, y in zip(a, b))  # + 100
+            print("The dot product of these vectors is: ", dot_product)
+            return dot_product
+        except TypeError or UnboundLocalError:
+            print("TypeError or UnboundLocalError (dot): You did not enter the values in the correct format. Please run the program again")
 
-        print("The dot product of these vectors is: ", dot_product)
 
-        return dot_product
+
+
 
 
     # getX()     - returns the x coordinate
 
     def getX(self):
+        """
+            Returns the x coordinate
+
+                Parameters:
+                    None
+
+                Returns:
+                    X coordinate of the instanced vector
+        """
         return self.x
 
 
     # getY()     - returns the y coordinate
 
     def getY(self):
+        """
+            Returns the y coordinate
+
+                Parameters:
+                    None
+
+                Returns:
+                    Y coordinate of the instanced vector
+        """
         return self.y
 
 
     # getZ()     - returns the z coordinate
 
     def getZ(self):
+        """
+            Returns the z coordinate
+
+                Parameters:
+                    None
+
+                Returns:
+                    Z coordinate of the instanced vector
+        """
         return self.z
 
 
     # input()    - handle inputting the coordinates from the user
 
     def input(self):
+        """
+            handle inputting the coordinates from the user
+
+                Parameters:
+                    None
+
+                Returns:
+                    Returns the user input in string format
+        """
 
         while True:
 
             user_input = input("Please enter the coordinates of the first vector: ")
 
-            if user_input == '-1':
-                break
+            if user_input == "-1":
+                quit()
 
             try:
                 int_list = [int(i) for i in user_input.split(" ")]
                 break
-                #print("Printing int list: ", int_list)
+
 
             except ValueError:
-                print("ValueError: That's not an integer. Please try to input numbers separated by a space")
+                print("ValueError(input): That's not an integer. Please try to input numbers separated by a space")
 
 
 
@@ -85,16 +153,39 @@ class Vector3D:
     # length()   - returns the length of the vector
 
     def length(self, b):
-        a = (self.x, self.y, self.z)
-        len_a, len_b = (math.sqrt(sum(i ** 2 for i in a)), math.sqrt(sum(i ** 2 for i in b)))
+        """
+            Calculates the length of the vector in its instance with the vector passed
 
-        return len_a, len_b
+                Parameters:
+                    The x, y, z coordinates of the second 3D vector
+
+                Returns:
+                    Calculated length of the two vectors
+        """
+        a = (self.x, self.y, self.z)
+
+        try:
+            len_a, len_b = (math.sqrt(sum(i ** 2 for i in a)), math.sqrt(sum(i ** 2 for i in b)))
+
+            return len_a, len_b
+
+        except TypeError:
+            print("TypeError (Length): You did not enter the values in the correct format. Please run the program again")
 
 
     # output()   - prints the vector out to the screen neatly. Eg. “(1, 2, 3)”
 
     def output(self, first_input, second_input):
-        print("The entered vectors are (from within the output function:")
+        """
+            prints the vector out to the screen neatly
+
+                Parameters:
+                    First and second vector inputs
+
+                Returns:
+                    None
+        """
+        print("The entered vectors are (from within the output function):")
 
         print("first input: ", first_input)
         print("second input: ", second_input)
@@ -103,13 +194,24 @@ class Vector3D:
     # set()      - set the coordinates of the vector
 
     def set(self, user_input):
+        """
+            set the coordinates of the vector
+
+                Parameters:
+                    Takes in the string input from the user and assigns it to initialized variables in constructor
+
+                Returns:
+                    Returns a neat integer tuple of the set vector
+        """
 
         parsed_user_input = user_input.split(" ")
+        try:
+            user_input_list = [int(value) for value in parsed_user_input]
+            user_input_tuple = tuple(user_input_list)
 
-        user_input_list = [int(value) for value in parsed_user_input]
-        user_input_tuple = tuple(user_input_list)
-
-        self.x, self.y, self.z = user_input_tuple
+            self.x, self.y, self.z = user_input_tuple
+        except ValueError:
+            print("ValueError (set): You did not enter the values in the correct format. Please run the program again")
 
         return user_input_tuple
 
@@ -128,21 +230,28 @@ def main():
 
     first_input = first_instance.input()   # asking the user to enter two vectors
     second_input = second_instance.input()   # asking the user to enter two vectors
-    #print("First input = ", first_input)
+
 
     first_input = first_instance.set(first_input)
     second_input = second_instance.set(second_input)
 
 
+
     dot_prod = first_instance.dot(second_input)
-    print("Dot prod: ", dot_prod)
 
     cross_prod = first_instance.cross(second_input)
-    print("Cross prod: ", cross_prod)
 
-    length_a, length_b = first_instance.length(second_input)    # will using first_instance.length(second_instance.input()) also work?
 
-    print("Length = ", length_a, "and ", length_b)
+    length = first_instance.length(second_input)    # will using first_instance.length(second_instance.input()) also work?
+    try:
+        length_a = length[0]
+        length_b = length[1]
+        print("Length = ", length_a, "and ", length_b)
+    except Exception:
+        print("You did not enter the values in the correct format. Please run the program again")
+
+
+
 
     first_instance.output(first_input, second_input)
 
--- test_question_2_revised.py
+++ test_question_2_revised.py
@@ -1,13 +1,14 @@
 import pytest
-
+import mock         #Is there a way to autmatically script installing this package?
 from question_2_revised import Vector3D
 import numpy as np
 from unittest.mock import Mock, patch, mock_open
+from io import StringIO
 import builtins
 import contextlib, io
 
 
-
+# pytest test_question_2_revised.py --cov-report term-missing --cov -v
 # pytest test_question_2_revised.py::test_cross -s
 def test_cross():
     first_trial = Vector3D(1 , 2, 3)
@@ -52,10 +53,19 @@ def test_getZ():
     test_output = first_trial.getZ()
     assert test_output == 3
 
+
 # pytest test_question_2_revised.py::test_input_works -s
-def test_input_works():
-    first_trial = Vector3D(None, None, None)
-    with pytest.raises(ValueError): first_trial.set('1 2 a')
+# @patch('builtins.input', return_value="yes")
+# def test_input_works(mock_input):
+#     with patch('sys.stdout', new=StringIO()) as mock_stdout:
+#         first_trial = Vector3D(1, 2, 3)
+#         first_trial.input()
+#         self.assertEqual(mock_stdout.getvalue().strip(), "Quitter!")
+#
+#     pass
+
+
+
 
 #pytest test_question_2_revised.py --cov-report term-missing --cov -v
 # pytest test_question_2_revised.py::test_length -s
@@ -69,3 +79,58 @@ def test_length():
     length_a, length_b = first_trial.length(second_test_input)
     assert length_a == 3.7416573867739413 and length_b == 8.774964387392123
 
+# pytest test_question_2_revised.py::test_set_works -v
+def test_set_works():
+    first_trial = Vector3D(1, 2, 3)
+    with pytest.raises(Exception):
+        first_trial.set('4 5 a')
+
+# pytest test_question_2_revised.py::test_cross_exception_works -v
+def test_cross_exception_works():
+    first_trial = Vector3D(1, 2, 3)
+    second_trial = Vector3D(None, None, None)
+    # first_trial.cross((4,))
+    with pytest.raises(Exception):
+        first_trial.cross((4, 5))
+
+
+# pytest test_question_2_revised.py::test_dot_works -v
+def test_dot_works():
+    first_trial = Vector3D(1, 2, 3)
+    with pytest.raises(TypeError):
+        first_trial.dot((4))
+
+# pytest test_question_2_revised.py::test_length_works -v
+def test_length_works():
+    first_trial = Vector3D(1, 2, 3)
+    with pytest.raises(TypeError):
+        first_trial.length((4))
+
+# pytest test_question_2_revised.py::test_output_works -v
+def test_output_works():
+    first_trial = Vector3D(1, 2, 3)
+    second_trial = Vector3D(4, 5, 6)
+
+    first_input = (1, 2, 3)
+    second_input = (4, 5, 6)
+    # print(first_trial.output(a))
+
+    # test_file = first_trial.output()
+    mock = Mock()
+    mock.side_effect = print  # ensure actual print is called to capture its txt
+    print_original = print
+    builtins.print = mock
+
+    try:
+        str_io = io.StringIO()
+        with contextlib.redirect_stdout(str_io):
+            first_trial.output(first_input, second_input)
+        output = str_io.getvalue()
+
+        assert print.called  # `called` is a Mock attribute
+        assert output.startswith("The entered vectors ")
+
+    finally:
+        builtins.print = print_original  # ensure print is "unmocked"
+
+# pytest test_question_2_revised.py --cov-report term-missing --cov -v
